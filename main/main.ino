bb086b5f860831c459e608683de2dbedb678e8d4
gargvatsal <vatsalgarg1997@gmail.com>
2021-11-04 16:44:46 +0530

    added the required libraries and code for wifi connection and than sending data to Thingspeak

--- main/main.ino
+++ main/main.ino
@@ -1,18 +1,42 @@
 // using HC SR-04 to get the measurement
 // should be able to create distance as of now
 
+#include <WiFi.h>
+#include "secret.h"
+#include "ThingSpeak.h"
+
 const int ECHOpin = 19;
 const int TRIGpin = 5;
 
 long int duration;  // stores the time, ECHOpin was HIGH
 float distance;   // stores the distance 
 
+WiFiClient client;
 void setup() {
   pinMode(ECHOpin, INPUT);
   pinMode(TRIGpin, OUTPUT);
 
   Serial.begin(115200);
-  Serial.println("Starting the Serial Monitor");
+  delay(10);
+
+  Serial.println();
+  Serial.println();
+  
+  Serial.println("Connecting to ");
+  Serial.println(USERNAME);
+
+  WiFi.begin(USERNAME, PASSWORD);
+
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+    }
+
+  Serial.println();
+  Serial.println("WiFi Connected");
+  Serial.println(WiFi.localIP());
+
+  ThingSpeak.begin(client);
 }
 
 void loop() {
@@ -24,13 +48,20 @@ void loop() {
 
   // pulseIn will be calulating the time for which ECHOpin will be HIGH
   duration = pulseIn(ECHOpin, HIGH); 
-  Serial.println(duration);
+  // Serial.println(float(duration)/60000000); // converting to minutes
   // distance = speed * time and divided by 2 to get one way distance
   distance = duration * 0.034 / 2;
 
   Serial.print("The measured distance is -- " );
   Serial.print(distance);
   Serial.println(" cm");
+
+  // setting fields in ThingSpeak
+  ThingSpeak.setField(1, distance);
+  ThingSpeak.setField(2, float(duration)/60000000); // converting to minutes
+
+  // Writting the values to ThinSpeak using the channel ID and API key
+  ThingSpeak.writeFields(CH_ID, API_KEY);
   
   delay(1000);
 }
---
+++ main/secret.h
@@ -0,0 +1,7 @@
+// replace the following with the actual details
+
+#define USERNAME "MySSID"
+#define PASSWORD "MyPassword"
+
+#define CH_ID 000000
+#define API_KEY "XYZ"
